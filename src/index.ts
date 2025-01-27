a9bd9dc9acd06a9b097fece168ac6c891ea990ce
eita <>
2025-01-27 15:25:18 +0900

    CORS対応

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 
 const app = new Hono()
 
@@ -6,4 +7,10 @@ app.get('/', (c) => {
   return c.json({ message: 'Hono MSG' })
 })
 
+app.use('*', cors({
+  origin: '*',
+  allowMethods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowHeaders: ['Content-Type', 'Authorization'],
+  maxAge: 600,
+}))
 export default app
