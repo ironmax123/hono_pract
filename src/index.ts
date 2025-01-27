b31fe7d10825019aff570705d3766b0821b36268
eita <>
2025-01-27 15:29:21 +0900

    修正

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ import { cors } from 'hono/cors'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.json({ message: 'Hono MSG' })
-})
-
 app.use('*', cors({
   origin: '*',
   allowMethods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowHeaders: ['Content-Type', 'Authorization'],
   maxAge: 600,
 }))
+
+app.get('/', (c) => {
+  return c.json({ message: 'Hono MSG' })
+})
 export default app
