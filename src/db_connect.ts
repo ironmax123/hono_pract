b3e19b6e04b08383c6f8afc6d13ecb5d4ed4c3d2
eita <>
2025-01-29 00:56:53 +0900

    修正

--- src/db_connect.ts
+++ src/db_connect.ts
@@ -3,10 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const SUPABASE_URLs = process.env.SUPABASE_URL||'';
-const SUPABASE_API_KEYs = process.env.SUPABASE_API_KEY||'';
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_API_KEY = process.env.SUPABASE_API_KEY;
+
+if (!SUPABASE_URL || !SUPABASE_API_KEY) {
+  throw new Error("Missing SUPABASE_URL or SUPABASE_API_KEY environment variable");
+}
 
 export const supabase = createClient(
-    SUPABASE_URLs,
-    SUPABASE_API_KEYs
+    SUPABASE_URL,
+    SUPABASE_API_KEY
 );
