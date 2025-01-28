fd472aecb355c8fe85ae45bbd0aa1f0f6f185600
eita <>
2025-01-29 00:57:56 +0900

    修正

--- src/db_connect.ts
+++ src/db_connect.ts
@@ -1,7 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_API_KEY = process.env.SUPABASE_API_KEY;
