f8e6e509122a253049c65476fc926a1d7182a6c2
eita <>
2025-01-29 00:59:05 +0900

    変更

--- src/db_connect.ts
+++ src/db_connect.ts
@@ -1,11 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_API_KEY = process.env.SUPABASE_API_KEY;
+const SUPABASE_URL = process.env.SUPABASE_URL||'';
+const SUPABASE_API_KEY = process.env.SUPABASE_API_KEY||'';
 
-if (!SUPABASE_URL || !SUPABASE_API_KEY) {
-  throw new Error("Missing SUPABASE_URL or SUPABASE_API_KEY environment variable");
-}
 
 export const supabase = createClient(
     SUPABASE_URL,
