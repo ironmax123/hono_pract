3edf38597bf0ec92e0b45899b749715378be6212
eita <>
2025-01-29 00:14:03 +0900

    エラーハンドリング

--- src/db_connect.ts
+++ src/db_connect.ts
@@ -3,8 +3,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const SUPABASE_URL = process.env.SUPABASE_URL as string;
-const SUPABASE_API_KEY = process.env.SUPABASE_API_KEY as string;
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_API_KEY = process.env.SUPABASE_API_KEY;
+
+if (!SUPABASE_URL || !SUPABASE_API_KEY) {
+    throw new Error("Missing SUPABASE_URL or SUPABASE_API_KEY environment variable");
+}
+
 export const supabase = createClient(
     SUPABASE_URL,
     SUPABASE_API_KEY
