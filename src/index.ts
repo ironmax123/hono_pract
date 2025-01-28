4d0f318fe18affc0bfd5e6f8fa6d5db9b1307f6f
eita <>
2025-01-29 00:00:12 +0900

    supabase Read

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
+import { supabase } from "./db_connect";
+import { useState } from 'hono/jsx';
 
 const app = new Hono()
+const [users, setUsers] = useState([""])
 
 app.use('*', cors({
   origin: '*',
@@ -13,4 +16,20 @@ app.use('*', cors({
 app.get('/', (c) => {
   return c.json({ message: 'HELLO WORLD' ,FROM:'from Hono on Cloudflare Workers!'})
 })
+app.get('/db', async (c) => {
+  const datas = await fetchUsers();
+  return c.json({ name: datas });
+})
+
 export default app
+
+async function fetchUsers() {
+  const { data } = await supabase
+    .from('users')
+    .select('name')
+  if (data) {
+    setUsers(data.map(user => user.name)) // useStateにデータを保存する
+    return data.map(user => user.name);
+  }
+  return [];
+}
