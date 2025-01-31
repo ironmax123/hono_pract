ecc69c962a316f68aca7327c312985f2f2f040f5
eita <>
2025-02-01 00:06:25 +0900

    うまくできたよ

--- src/db_connect.ts
+++
@@ -1,5 +0,0 @@
-export {}
-declare global {
-  const SUPABASE_API_KEY: string;
-  const SUPABASE_URL: string;
-}
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import { useState } from 'hono/jsx';
 const app = new Hono()
 const [users, setUsers] = useState([""])
 
+
+
 app.use('*', cors({
   origin: '*',
   allowMethods: ['GET', 'POST', 'PUT', 'DELETE'],
@@ -20,25 +22,27 @@ app.get('/msg', (c) => {
   return c.json({ message: 'Hono!' ,FROM:'Cloudflare'})
 })
 app.get('/db', async (c) => {
-  const datas = await fetchUsers();
+  const SUPABASE_URL = (c.env as { SUPABASE_URL: string }).SUPABASE_URL;
+  const SUPABASE_API_KEY = (c.env as { SUPABASE_API_KEY: string }).SUPABASE_API_KEY;
+  const datas = await fetchUsers(SUPABASE_URL,SUPABASE_API_KEY);
   console.log(datas);
-  return c.json({ name: datas });
+  
+  return c.json({ users: datas});
 })
 
 export default app
 
-async function fetchUsers() {
+async function fetchUsers(SUPABASE_URL: string, SUPABASE_API_KEY: string) {
   const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
   try {
     const { data, error } = await supabase
       .from('users')
-      .select('names')
+      .select('*')
     if (error) {
       throw error;
     }
     if (data) {
-      setUsers(data.map(user => user.names)) // useStateにデータを保存する
-      return data.map(user => user.names);
+      return data;
     }
     return [];
   } catch (error) {
