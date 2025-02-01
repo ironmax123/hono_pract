67236db3b2807a60d56b7d4e232f5ca92383fbd0
eita <>
2025-02-01 16:30:55 +0900

    修正

--- src/index.ts
+++ src/index.ts
@@ -53,7 +53,8 @@ app.post('/db/updateUser' ,async (c) => {
   const SUPABASE_URL = (c.env as { SUPABASE_URL: string }).SUPABASE_URL;
   const SUPABASE_API_KEY = (c.env as { SUPABASE_API_KEY: string }).SUPABASE_API_KEY;
   const { name } = await c.req.json();
-  const result = await updateUser(SUPABASE_URL, SUPABASE_API_KEY, name);
+  const { newName } = await c.req.json();
+  const result = await updateUser(SUPABASE_URL, SUPABASE_API_KEY, name,newName);
   return c.json({ result: result});
 })
 export default app
@@ -120,13 +121,14 @@ async function deleteUser(SUPABASE_URL: string, SUPABASE_API_KEY: string,name:St
   }
 }
 
-async function updateUser(SUPABASE_URL: string, SUPABASE_API_KEY: string,name:String){
+async function updateUser(SUPABASE_URL: string, SUPABASE_API_KEY: string,name:String,newName:String){
   const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
   
   try {
     const { data, error } = await supabase
       .from('users')
-      .update({ names: name })
+      .update({ names: newName })
+      .eq('names', name)
 
     if (error) {
       console.error('Supabase error:', error);
