091799e551045d19129c5521110860bac457fae4
eita <>
2025-02-01 16:15:57 +0900

    削除処理

--- src/index.ts
+++ src/index.ts
@@ -40,6 +40,14 @@ app.post('/db/addUser', async (c) => {
   const result = await addUser(SUPABASE_URL, SUPABASE_API_KEY, name);
   return c.json({ result: result});
 })
+
+app.get('/db/deleteUser' ,async (c) => {
+  const SUPABASE_URL = (c.env as { SUPABASE_URL: string }).SUPABASE_URL;
+  const SUPABASE_API_KEY = (c.env as { SUPABASE_API_KEY: string }).SUPABASE_API_KEY;
+  const { name } = await c.req.json();
+  const result = await deleteUser(SUPABASE_URL, SUPABASE_API_KEY, name);
+  return c.json({ result: result});
+})
 export default app
 
 
@@ -70,6 +78,27 @@ async function addUser(SUPABASE_URL: string, SUPABASE_API_KEY: string,name:Strin
     const { data, error } = await supabase
       .from('users')
       .insert({ names: name})
+
+    if (error) {
+      console.error('Supabase error:', error);
+      throw error;
+    }
+
+    return '成功しました';
+  } catch (error) {
+    console.error('Error fetching user names:', error);
+    return [];
+  }
+}
+
+async function deleteUser(SUPABASE_URL: string, SUPABASE_API_KEY: string,name:String){
+  const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
+  
+  try {
+    const { data, error } = await supabase
+      .from('users')
+      .delete()
+      .eq('names', name)
 
     if (error) {
       console.error('Supabase error:', error);
